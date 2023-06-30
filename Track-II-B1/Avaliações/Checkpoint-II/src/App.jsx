dbcedd4eec93c4e75daa7a92bdb64525bd8a01b2
Matheus Ferreira <matheusrbferreira@gmail.com>
2023-06-29 22:03:57 -0300

    Inclusão/ajuste de dependencias Adição da store configuração do thunk

--- Track-II-B1/Avaliações/Checkpoint-II/src/App.jsx
+++ Track-II-B1/Avaliações/Checkpoint-II/src/App.jsx
@@ -5,16 +5,20 @@ import PaginaInicio from "./paginas/Inicio.pagina";
 import PaginaFavoritos from "./paginas/Favoritos.pagina";
 import PaginaDetalhe from "./paginas/Detalhe.pagina";
 import Cabecalho from "./componentes/layout/cabecalho.componente";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
     <div className="App">
-      <Cabecalho />
-      <Routes>
-        <Route path="/" element={<PaginaInicio />} />
-        <Route path="favoritos" element={<PaginaFavoritos />} />
-        <Route path="detalhe" element={<PaginaDetalhe />} />
-      </Routes>
+      <Provider store={store}>
+        <Cabecalho />
+        <Routes>
+          <Route path="/" element={<PaginaInicio />} />
+          <Route path="favoritos" element={<PaginaFavoritos />} />
+          <Route path="detalhe" element={<PaginaDetalhe />} />
+        </Routes>
+      </Provider>
     </div>
   );
 }
---
+++ Track-II-B1/Avaliações/Checkpoint-II/src/redux/store.ts
@@ -0,0 +1,23 @@
+import {combineReducers} from "@reduxjs/toolkit";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// Importamos applyMiddleware do Redux, para poder adicionar Thunk ou Saga como Middleware
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import {TypedUseSelectorHook, useSelector as useReduxSelector} from "react-redux";
+
+// Importamos o thunk de redux-thunk
+import thunk from 'redux-thunk';
+
+
+const rootReducer = combineReducers({
+
+});
+
+export type IRootState = ReturnType<typeof rootReducer>;
+
+// Tipamos o hook useSelector
+export const useSelector: TypedUseSelectorHook<IRootState> = useReduxSelector
+
+export const store = createStore(
+    rootReducer, composeWithDevTools(applyMiddleware(thunk))
+)
